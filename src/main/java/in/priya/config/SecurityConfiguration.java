b43183bbf89cfaeebca80551a31f0ddbef74fc4d
Priyanka <priyasuryawanshi229@gmail.com>
2024-02-07 15:43:45 +0530

    updated

--- src/main/java/in/priya/config/SecurityConfiguration.java
+++ src/main/java/in/priya/config/SecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +24,21 @@ public class SecurityConfiguration {
 		return http.build();
 	}
 	
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUser()
+	{
+		UserDetails priyaUser=User.withDefaultPasswordEncoder().username("priya")
+				                                                .password("Priya@123")
+				                                                .authorities("ADMIN")
+				                                                .build();
+		
+		UserDetails snehalUser=User.withDefaultPasswordEncoder().username("snehal")
+				                                                 .password("Snehal@123")
+				                                                 .authorities("USER")
+				                                                 .build();
+		                                                          
+		
+		return new InMemoryUserDetailsManager(priyaUser,snehalUser);
+	}
 
 }
